d050f1865492e8022d8471d19ecfd16d793c9325
Yslan Ramos <yslan3000@gmail.com>
2021-02-22 19:31:36 -0300

    sum up all values read from the file

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -5,6 +5,14 @@ defmodule Rocketpay.Numbers do
       |> handle_file
   end
 
-  defp handle_file({:ok, file}), do: file
+  defp handle_file({:ok, file}) do
+    result = file
+      |> String.split(",")
+      |> Enum.map(fn n -> String.to_integer(n) end)
+      |> Enum.sum
+
+    {:ok, %{ result: result }}
+  end
+
   defp handle_file({:error, _reason}), do: {:error, "invalid file!"}
 end
