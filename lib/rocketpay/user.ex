8ebedae448ed7fe9b63728b5d4c843fb6be2e5bc
Yslan Ramos <yslan3000@gmail.com>
2021-02-23 10:57:41 -0300

    add validate to user schema

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -17,5 +17,10 @@ defmodule Rocketpay.User do
     %__MODULE__{}
      |> cast(params, @required_params)
      |> validate_required(@required_params)
+     |> validate_length(:passowrd_hash, min: 6)
+     |> validate_number(:age, greater_than_or_equal_to: 18)
+     |> validate_format(:email, ~r/@/)
+     |> unique_constraint([:email])
+     |> unique_constraint([:nickname])
   end
 end
